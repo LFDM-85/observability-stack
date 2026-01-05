8697fd0d9dfbd435a8571f01818fe0260f784958
Luís Melo <luis.f.duraes.melo@gmail.com>
2026-01-05 22:55:34 +0000

    Added automatic dashboards for grafana

--- scripts/download_dashboard.py
+++ scripts/download_dashboard.py
@@ -1,15 +1,21 @@
 import urllib.request
 import os
 
-url = "https://raw.githubusercontent.com/rfmoz/grafana-dashboards/master/prometheus/node-exporter-full.json"
-dest_dir = os.path.join(os.path.dirname(__file__), "../grafana/dashboards")
-dest_file = os.path.join(dest_dir, "node_exporter_full.json")
+DASHBOARDS = {
+    "node_exporter_full.json": "https://raw.githubusercontent.com/rfmoz/grafana-dashboards/master/prometheus/node-exporter-full.json",
+    "docker_containers.json": "https://raw.githubusercontent.com/fcwu/docker-dashboard-cadvisor-grafana/master/Grafana_Dashboard.json",
+    "prometheus_overview.json": "https://grafana.com/api/dashboards/3662/revisions/2/download",
+    "alloy_overview.json": "https://grafana.com/api/dashboards/21245/revisions/1/download"
+}
 
+dest_dir = os.path.join(os.path.dirname(__file__), "../grafana/dashboards")
 os.makedirs(dest_dir, exist_ok=True)
 
-try:
-    print(f"Downloading {url}...")
-    urllib.request.urlretrieve(url, dest_file)
-    print(f"Saved to {dest_file}")
-except Exception as e:
-    print(f"Error: {e}")
+for filename, url in DASHBOARDS.items():
+    dest_file = os.path.join(dest_dir, filename)
+    try:
+        print(f"Downloading {filename} from {url}...")
+        urllib.request.urlretrieve(url, dest_file)
+        print(f"Saved to {dest_file}")
+    except Exception as e:
+        print(f"Error downloading {filename}: {e}")
